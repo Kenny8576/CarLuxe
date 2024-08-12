b9d692632bdb145130df9c921ac8048123e78f1a
Kehinde Olubiyi <olubiyipeter01@gmail.com>
2024-08-12 20:43:08 +0100

    End of Section 3

--- src/AuctionService/Controllers/AuctionControllers.cs
+++ src/AuctionService/Controllers/AuctionControllers.cs
@@ -3,6 +3,7 @@ using AuctionService.Data;
 using AuctionService.DTOs;
 using AuctionService.Entities;
 using AutoMapper;
+using AutoMapper.QueryableExtensions;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -23,14 +24,20 @@ public class AuctionController : ControllerBase
     }
 
     [HttpGet]
-    public async Task<ActionResult<List<AuctionDtos>>> GetAllAuction()
+    public async Task<ActionResult<List<AuctionDtos>>> GetAllAuction(string date)
     {
-        var auctions = await _context.Auctions
-                        .Include(x => x.Item)
-                        .OrderByDescending(x => x.Item.Make)
-                        .ToListAsync();
+        var query = _context.Auctions.OrderBy(x => x.Item.Make).AsQueryable();
 
-        return _mapper.Map<List<AuctionDtos>>(auctions);
+        if(!string.IsNullOrEmpty(date))
+        {
+            query = query.Where(x => x.UpdatedAt.CompareTo(DateTime.Parse(date).ToUniversalTime()) > 0);
+        }
+        // var auctions = await _context.Auctions
+        //                 .Include(x => x.Item)
+        //                 .OrderByDescending(x => x.Item.Make)
+        //                 .ToListAsync();
+
+        return await query.ProjectTo<AuctionDtos>(_mapper.ConfigurationProvider).ToListAsync();
     }
 
     [HttpGet("{id}")]
--- src/AuctionService/Migrations/AuctionDbContextModelSnapshot.cs
+++ src/AuctionService/Migrations/AuctionDbContextModelSnapshot.cs
@@ -3,20 +3,17 @@ using System;
 using AuctionService.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
-using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 #nullable disable
 
-namespace AuctionService.Data.Migrations
+namespace AuctionService.Migrations
 {
     [DbContext(typeof(AuctionDbContext))]
-    [Migration("20240810102434_InitialCreate")]
-    partial class InitialCreate
+    partial class AuctionDbContextModelSnapshot : ModelSnapshot
     {
-        /// <inheritdoc />
-        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        protected override void BuildModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
